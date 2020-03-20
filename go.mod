aa7d2228f9e9280b9fd1cd47855df4a51dea7f15
Freckie <freckie@frec.kr>
2020-03-20 22:03:23 +0900

    Fix: host bug

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.13
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/websocket v1.4.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/rs/cors v1.7.0
 )
--- main.go
+++ main.go
@@ -8,9 +8,25 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rs/cors"
 )
 
 var addr = flag.String("addr", ":8082", "http service address")
+
+type HostSwitch map[string]http.Handler
+
+func (hs HostSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if handler := hs[r.Host]; handler != nil {
+		ip := r.RemoteAddr
+		log.Println("[Req]", r.Method, r.URL, ip)
+		handler.ServeHTTP(w, r)
+	} else {
+		http.Error(w, "Forbidden hostname : "+r.Host, http.StatusForbidden)
+		return
+	}
+}
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
@@ -32,11 +48,19 @@ func main() {
 
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+
+	router := httprouter.New()
+	router.HandlerFunc("GET", "/", serveHome)
+	router.HandlerFunc("GET", "/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
-	err := http.ListenAndServe(*addr, nil)
+
+	handler := cors.AllowAll().Handler(router)
+	hs := make(HostSwitch)
+	hs["chat-api"] = handler
+	hs["localhost"+*addr] = handler
+
+	err := http.ListenAndServe(*addr, handler)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
